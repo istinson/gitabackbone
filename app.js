bb97b380859bca39e11913f57338033a1ef10833
fogcity89 <kevinmtow@gmail.com>
2016-05-23 16:35:48 -0700

    fixed errors

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-<<<<<<< 028987435129839dd3a4df380daf5714f94d6753
 var app = angular.module('kirk');
-=======
-
-var express = require('express');
-var app = express();
-
-var app = angular.module('');
-
->>>>>>> erver.js
 
 app.config(function($routeProvider, $httpProvider) {
   $routeProvider.when(
